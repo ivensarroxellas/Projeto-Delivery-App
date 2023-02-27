647812f0bec3f0e03dcbb16ec02c1f2d7d19f416
Hugo <huleonardo19@gmail.com>
2023-02-27 16:25:30 -0300

    feat: implementa rotas de login e register no app.js.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import './App.css';
-import rockGlass from './images/rockGlass.svg';
+import { Route, Routes } from 'react-router-dom';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
-    <div className="App">
-      <span className="logo">TRYBE</span>
-      <object className="rocksGlass" type="image/svg+xml" data={ rockGlass }>
-        Glass
-      </object>
-    </div>
+    <Routes>
+      <Route path="/login" element={ <Login /> } />
+      <Route path="/register" element={ <Register /> } />
+    </Routes>
   );
 }
 
