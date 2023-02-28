432db5b89fa2830ed1cd09c5ea6412ced041bff5
Ivanzmartins <ivanzmartins@gmail.com>
2023-02-27 21:03:04 -0300

    adiciona seeders para as tabelas users e products

--- back-end/src/database/seeders/20230227204458-user.js
+++ back-end/src/database/seeders/20230227204458-user.js
@@ -1,24 +1,33 @@
 'use strict';
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('users', [
+      {
+        id: 1,
+        name: 'Delivery App Admin',
+        email: 'adm@deliveryapp.com',
+        password: 'a4c86edecc5aee06eff8fdeda69e0d04',
+        role: 'administrator',
+      },
+      {
+        id: 2,
+        name: 'Fulana Pereira',
+        email: 'fulana@deliveryapp.com',
+        password: '3c28d2b0881bf46457a853e0b07531c6',
+        role: 'seller',
+      },
+      {
+        id: 3,
+        name: 'Cliente Zé Birita',
+        email: 'zebirita@email.com',
+        password: '1c37466c159755ce1fa181bd247cb925',
+        role: 'customer',
+      },
+    ], {});
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('users', null, {});
   }
 };
